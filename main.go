860f3adb4eecb896a2da2cdf38d45c7b2ff912cb
Andre <andrebrreis@gmail.com>
2018-10-21 13:37:54 +0200

    Fix typos

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package MyFirstNeoGoContract
 
 import (
-	"github.com/CityOfZion/neo-storm/examples/token/nep5"
-	"github.com/CityOfZion/neo-storm/interop/runtime"
-	"github.com/CityOfZion/neo-storm/interop/storage"
-	"github.com/CityOfZion/neo-storm/interop/util"
-	"github.com/CityOfZion/neo-storm/interop/runtime"	
+	"neo-storm/token/nep5"
+	"neo-storm/interop/runtime"
+	"neo-storm/interop/engine"
+	"neo-storm/interop/storage"
+	"neo-storm/interop/util"
 )
 
 const (
@@ -13,7 +13,6 @@ const (
 	multiplier = 100000000
 )
 
-const VERIFICATION_R = 0x01
 
 // CreateToken initializes the Token Interface for the Smart Contract to operate with
 func CreateToken() nep5.Token {
@@ -30,6 +29,7 @@ func CreateToken() nep5.Token {
 func Main(operation string, args []interface{}) bool {
 	token := CreateToken()
 	trigger := runtime.GetTrigger()
+	productHash := args[0].([]byte)
 
 	if trigger == runtime.Application() {
 		if operation == "create_offer" {
@@ -37,7 +37,7 @@ func Main(operation string, args []interface{}) bool {
 			if checkArgs(args, 2) {
 				return false
 			}
-			return create_offer(args)
+			return createOffer(args)
 		}
 		if operation == "reject_application" {
 			//TODO:
@@ -45,23 +45,18 @@ func Main(operation string, args []interface{}) bool {
 		}
 		if operation == "confirm_purchase" {
 			//TODO check if sender is the buyer
-			buyerPre := []byte("BUYER")
-			buyerKey := append(buyerPre, productHash...)
-			buyer := storage.Get(storage.GetContext(), buyerKey)
+			buyer := retrieveBuyerAddress(productHash)
 			validateCallingAddress(buyer)
 
-			productHash := args[0].([]byte)
 
 			amountPre := []byte("AMOUNT")
 			amountKey := append(amountPre, productHash...)
 			productCost := storage.Get(storage.GetContext(), amountKey)
 
-			sellerPre := []byte("SELLER")
-			sellerKey := append(sellerPre, productHash...)
-			seller := storage.Get(storage.GetContext(), sellerKey)
+			seller := retrieveSellerAddress(productHash)
 
-			token.Transfer(storage.GetContext(), engine.GetExecutingScriptHash(), buyer.([]byte),  1/4*productCost.(int))
-			token.Transfer(storage.GetContext(), engine.GetExecutingScriptHash(), seller.([]byte),  3/4*productCost.(int))
+			token.Transfer(storage.GetContext(), engine.GetExecutingScriptHash(), buyer,  1/4*productCost.(int))
+			token.Transfer(storage.GetContext(), engine.GetExecutingScriptHash(), seller,  3/4*productCost.(int))
 			return true
 		}
 
@@ -70,14 +65,14 @@ func Main(operation string, args []interface{}) bool {
 			if checkArgs(args, 2) {
 				return false
 			}
-			apply_to_offer(token, args)
+			applyToOffer(token, args)
 		}
 		if operation == "accept_offer" {
 			// Gas and buyer's contact info.
 			if checkArgs(args, 2) {
 				return false
 			}
-			accept_offer(token, args)
+			acceptOffer(token, args)
 		}
 	}
 	
@@ -87,7 +82,7 @@ func Main(operation string, args []interface{}) bool {
 
 
 // The seller puts an item on offer on the chain
-func create_offer(args []interface{}) bool {
+func createOffer(args []interface{}) bool {
 	ctx := storage.GetContext()
 
 	productHash := args[0].([]byte)
@@ -108,71 +103,41 @@ func create_offer(args []interface{}) bool {
 }
 
 // The buyer applies to seller's offer.
-func apply_to_offer(t Token, args []interface{}) bool {
-	amountSent := args[0]
+func applyToOffer(t nep5.Token, args []interface{}) bool {
 	
 	productHash := args[0].([]byte)
 	amountPre := []byte("AMOUNT")
 	amountKey := append(amountPre, productHash...)
-	productCost := storage.Get(storage.GetContext(), amountKey)
+	productCost := storage.Get(storage.GetContext(), amountKey).(int)
 
 
 	// Check if buyer has enough tokens.
-	buyerPre := []byte("BUYER")
-	buyerKey := append(buyerPre, productHash...)
-	buyer := storage.Get(storage.GetContext(), buyerKey)
+	buyer := retrieveBuyerAddress(productHash)
 
-	// Not enough tokens
-	if !token.canTransfer(storage.getContext, buyer([]byte), engine.GetCallingScriptHash(), amountSent) {
-		return false
+
+	if t.Transfer(storage.GetContext(), buyer, engine.GetExecutingScriptHash(), 2*productCost) {
+		buyerPre := []byte("BUYER")
+		buyerKey := append(buyerPre, productHash...)
+		storage.Put(storage.GetContext(), buyerKey, engine.GetCallingScriptHash())
+		return true
 	}
-
-	// Tokens don't match price + escrow
-	if amountSent != 2 * productCost.(int) { 
-		return false
-	}
-
-	token.tranfser(storage.getContext, buyer([]byte), engine.GetExecutingScriptHash(), amountSent)
-
-	buyerPre := []byte("BUYER")
-	buyerKey := append(buyerPre, productHash...)
-	storage.Put(storage.GetContext(), buyerKey, engine.GetCallingScriptHash())
-
-	//TODO put buyers address on the chain (args[1])
-
-	return true
-
+	return false
 }
 
 // The seller puts tokens in the escrow of the offer.
-func accept_offer(t Token, args []interface{}) bool {
-	amountSent := args[0]
+func acceptOffer(t nep5.Token, args []interface{}) bool {
+	productHash := args[0].([]byte)
 
 	//Validate if sender is the seller
-	sellerPre := []byte("SELLER")
-	sellerKey := append(sellerPre, productHash...)
-	seller := storage.Get(storage.GetContext(), sellerKey)
+	seller := retrieveSellerAddress(productHash)
 	validateCallingAddress(seller)
 
-	// Not enough tokens
-	if !token.canTransfer(storage.getContext, seller([]byte), engine.GetExecutingScriptHash(), amountSent) {
-		return false
-	}
-
-	productHash := args[0].([]byte)
 	amountPre := []byte("AMOUNT")
 	amountKey := append(amountPre, productHash...)
-	productCost := storage.Get(storage.GetContext(), amountKey)
-
-	// Tokens don't match price + escrow
-	if amountSent != 2 * productCost.(int) { 
-		return false
-	}
+	productCost := storage.Get(storage.GetContext(), amountKey).(int)
 
 	//Transfer tokens
-	token.tranfser(storage.getContext, seller([]byte), engine.GetExecutingScriptHash(), amountSent)
-
-	return true
+	return t.Transfer(storage.GetContext(), seller, engine.GetExecutingScriptHash(), 2*productCost)
 
 }
 
@@ -185,17 +150,18 @@ func checkArgs(args []interface{}, length int) bool {
 }
 
 func validateCallingAddress(desiredAddress []byte) bool {
-	return engine.GetCallingScriptHash == desiredAddress
+	return util.Equals(engine.GetCallingScriptHash, desiredAddress)
 }
 
-func retrieveSellerAddress() interface{} {
+func retrieveSellerAddress(productHash []byte) []byte {
 	sellerPre := []byte("SELLER")
 	sellerKey := append(sellerPre, productHash...)
-	seller := storage.Get(storage.GetContext(), sellerKey)
+	return storage.Get(storage.GetContext(), sellerKey).([]byte)
+	
 }
 
-func retrieveBuyerAddress() interface{} {
+func retrieveBuyerAddress(productHash []byte) []byte {
 	buyerPre := []byte("BUYER")
 	buyerKey := append(buyerPre, productHash...)
-	buyer := storage.Get(storage.GetContext(), buyerKey)
+	return storage.Get(storage.GetContext(), buyerKey).([]byte)
 }
